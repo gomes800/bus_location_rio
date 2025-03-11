257a4229c33c5a50f1e83933f35f3c6e24c12aa3
gomes <joaogomesleite800@gmail.com>
2025-03-11 07:45:52 -0300

    First commit

---
+++ src/main/java/com/gom/bus_location_rio/BusLocationRioApplication.java
@@ -0,0 +1,13 @@
+package com.gom.bus_location_rio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusLocationRioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusLocationRioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gom/bus_location_rio/BusLocationRioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom.bus_location_rio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusLocationRioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
